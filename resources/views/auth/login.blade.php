650d394f74a28325c28f4aea6961a779ece4e1a3
qhj1989qqcom <qhj1989@qq.com>
2018-11-23 19:38:38 +0800

    no message

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">注册</div>
+                <div class="panel-heading">登录</div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
